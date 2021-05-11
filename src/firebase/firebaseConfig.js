bdeb4a23aee8c80806d130bb43bc3967cea11605
Mayank52 <mayank.aggarwal963@gmail.com>
2021-05-11 19:58:48 +0530

    firebase update

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDn_yk-fbPujciOoDUUJdcK5R4XwAiwJ1s",
-  authDomain: "resume-builder-e58f0.firebaseapp.com",
-  databaseURL: "https://resume-builder-e58f0.firebaseio.com",
-  projectId: "resume-builder-e58f0",
-  storageBucket: "resume-builder-e58f0.appspot.com",
-  messagingSenderId: "8357925301",
-  appId: "1:8357925301:web:3111013cb56deb881b89fc",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 
